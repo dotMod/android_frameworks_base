dd82c5bc72360cdfe3706bd1bc31313078b29c12
Varun Date <date.varun123@gmail.com>
2020-05-29 16:21:16 +1000

    PowerManagerService: Catch em all
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: *** FATAL EXCEPTION IN SYSTEM PROCESS: PowerManagerService
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: java.lang.RuntimeException: Error receiving broadcast Intent { act=android.intent.action.BATTERY_CHANGED flg=0x60000010 (has extras) } in com.android.server.power.PowerManagerService$BatteryReceiver@d469668
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at android.app.LoadedApk$ReceiverDispatcher$Args.lambda$getRunnable$0$LoadedApk$ReceiverDispatcher$Args(LoadedApk.java:1565)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at android.app.-$$Lambda$LoadedApk$ReceiverDispatcher$Args$_BumDX2UKsnxLVrE6UJsJZkotuA.run(Unknown Source:2)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at android.os.Handler.handleCallback(Handler.java:883)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:100)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:214)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at android.os.HandlerThread.run(HandlerThread.java:67)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at com.android.server.ServiceThread.run(ServiceThread.java:44)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: Caused by: java.lang.SecurityException: uid 1000 does not have android.permission.RESET_BATTERY_STATS.
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at android.app.ContextImpl.enforce(ContextImpl.java:1896)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at android.app.ContextImpl.enforceCallingPermission(ContextImpl.java:1914)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at com.android.server.am.BatteryStatsService.resetStatistics(BatteryStatsService.java:432)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at com.android.server.power.PowerManagerService.updateSmartChargingStatus(PowerManagerService.java:2010)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at com.android.server.power.PowerManagerService.updateIsPoweredLocked(PowerManagerService.java:1989)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at com.android.server.power.PowerManagerService.updatePowerStateLocked(PowerManagerService.java:1860)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at com.android.server.power.PowerManagerService.handleBatteryStateChangedLocked(PowerManagerService.java:3089)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at com.android.server.power.PowerManagerService.access$2400(PowerManagerService.java:124)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at com.android.server.power.PowerManagerService$BatteryReceiver.onReceive(PowerManagerService.java:4175)
    05-28 19:02:22.754  1354  1411 E AndroidRuntime: 	at android.app.LoadedApk$ReceiverDispatcher$Args.lambda$getRunnable$0$LoadedApk$ReceiverDispatcher$Args(LoadedApk.java:1555)
    
    Change-Id: I3ac262f710cc287c13c4d10a5ec423aeb48d10b6
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -2290,7 +2290,7 @@ public final class PowerManagerService extends SystemService
                 if (suspendBySmartCharging && mSmartChargingResetStats) {
                     try {
                         mBatteryStats.resetStatistics();
-                    } catch (RemoteException e) {
+                    } catch (Exception e) {
                         Slog.e(TAG, "failed to reset battery statistics");
                     }
                 }
