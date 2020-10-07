8caa0b612db194a6f04d8f93e0cc4e0b05f66195
Danny Lin <danny@kdrag0n.dev>
2020-10-06 18:20:03 -0700

    SystemUI: Never animate on wakeup
    The slow fade-in wakeup animation is currently only skipped when opening
    the camera app using the power button shortcut. However, wakeups in
    general are time-critical operations, so let's disable the wakeup
    animation all the time to speed up the wakeup process.
    
    Change-Id: I9ba3fbee473588be1f2b5638a656fed1c9fdcf79

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
@@ -77,6 +77,7 @@ public enum ScrimState {
             } else {
                 mAnimationDuration = ScrimController.ANIMATION_DURATION;
             }
+            mAnimateChange = false;
             mFrontTint = Color.BLACK;
             mBehindTint = Color.BLACK;
             mBubbleTint = Color.TRANSPARENT;
@@ -216,7 +217,7 @@ public enum ScrimState {
                     ? mKeyguardFadingAwayDuration
                     : StatusBar.FADE_KEYGUARD_DURATION;
 
-            mAnimateChange = !mLaunchingAffordanceWithPreview;
+            mAnimateChange = false;
 
             mFrontTint = Color.TRANSPARENT;
             mBehindTint = Color.TRANSPARENT;
