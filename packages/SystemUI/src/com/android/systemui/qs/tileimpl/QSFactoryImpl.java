0143f61c61a0b6801e686780302164b2a043c246
DarkJoker360 <DarkJoker360@users.noreply.github.com>
2021-06-22 16:34:49 +0000

    SystemUI: Add a tile to show power menu * Following what swapsCAPS did for Volume tile, create a tile to show power menu that could come handy for people with accessibility probems over physic buttons.
    Change-Id: Ie30470ad90ce9db8f7687b2db05ab3c58d20a53d

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -48,6 +48,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
+import com.android.systemui.qs.tiles.PowerMenuTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.SoundTile;
@@ -102,6 +103,8 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<SoundTile> mSoundTileProvider;
     private final Provider<UsbTetherTile> mUsbTetherTileProvider;
     private final Provider<VpnTile> mVpnTileProvider;
+    private final Provider<PowerMenuTile> mPowerMenuTileProvider;
+
     private final Lazy<QSHost> mQsHostLazy;
 
     @Inject
@@ -137,7 +140,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<SoundTile> soundTileProvider,
             Provider<AntiFlickerTile> antiFlickerTileProvider,
             Provider<UsbTetherTile> usbTetherTileProvider,
-            Provider<VpnTile> vpnTileProvider) {
+            Provider<VpnTile> vpnTileProvider,
+            Provider<PowerMenuTile> powerMenuTileProvider) {
         mQsHostLazy = qsHostLazy;
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
@@ -171,6 +175,7 @@ public class QSFactoryImpl implements QSFactory {
         mAntiFlickerTileProvider = antiFlickerTileProvider;
         mUsbTetherTileProvider = usbTetherTileProvider;
         mVpnTileProvider = vpnTileProvider;
+        mPowerMenuTileProvider = powerMenuTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -246,6 +251,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mUsbTetherTileProvider.get();
             case "vpn":
                 return mVpnTileProvider.get();
+            case "powermenu":
+                return mPowerMenuTileProvider.get();
         }
 
         // Custom tiles
