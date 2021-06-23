18beddc5d349f6d897d119fc014dbcbe1e73cf44
root <root@raphael.ehp0n55wvzhu3m2fz1tduxiosh.lx.internal.cloudapp.net>
2021-06-23 12:23:05 +0000

    Revert "test"
    This reverts commit b0b6ce038e3c25610ff97a0f6604bdf4b8c33541.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/FPSInfoTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/FPSInfoTile.java
@@ -58,7 +58,7 @@ public class FPSInfoTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.CUSTOM_TILE;
+        return MetricsEvent.DOT_QS_TILES;
     }
 
     @Override
