52e571e5053c89b6e98c94456c7cd1e75540f1ef
root <root@raphael.ehp0n55wvzhu3m2fz1tduxiosh.lx.internal.cloudapp.net>
2021-06-26 13:05:18 +0000

    Revert "SystemUI: Nuke 2-button navbar"
    This reverts commit 03d8af337fd9156390372705f0f4faa5f2449c44.

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -127,6 +127,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconShapeTaperedRectOverlay \
 	IconShapeTeardropOverlay \
 	IconShapeVesselOverlay \
+	NavigationBarMode2ButtonOverlay \
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
 	NavigationBarModeGesturalOverlayNarrowBack \
